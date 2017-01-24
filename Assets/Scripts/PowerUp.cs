cdbd2000aecc201bcba16a91711c1cb42767c4af
Ethan Le <Ethan_le95@hotmail.com>
2017-01-23 20:19:38 -0500

    Working on Power up class
    Adding it's features.

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -7,14 +7,81 @@ public class PowerUp : MonoBehaviour
     protected float m_Speed;
     protected float m_StunTime;
 
+    private int m_Counter;
     private float m_Timer = 0;
 
-    protected virtual void SpawnObjectAtRandom(GameObject powerUp)
+    private int m_xPosition;
+    private int m_yPosition;
+
+    [SerializeField]
+    private GameObject missle;
+    [SerializeField]
+    private GameObject Arrow;
+    [SerializeField]
+    private GameObject Electricity;
+    [SerializeField]
+    private GameObject SpeedBoots;
+
+    private TileManager tile;
+
+    protected virtual void Start()
     {
-        //TODO Make a random generator to make the objects spawn at a random grid at a certain amount of time.
+        tile = GetComponent<TileManager>();
     }
-    
+
     protected virtual void Upate()
     {
     }
+
+    protected virtual void SpawnPowerUp()
+    {
+
+    }
+
+    //protected void SpawnMissle()
+    //{
+    //    if (m_Counter <= 1)
+    //    {
+    //        m_xPosition = Random.Range(0, 8);
+    //        m_xPosition = Random.Range(0, 8);
+
+    //        GameObject spawnMissle = (GameObject)Instantiate(missle);
+
+    //        spawnMissle.transform.position = tile.GetGridTilePosition(m_xPosition, m_xPosition);
+
+    //        m_Counter += 1;
+    //    }
+    //}
+
+    //protected void SpawnArrowDirection()
+    //{
+    //    if (m_Counter <= 3)
+    //    {
+    //        m_xPosition = Random.Range(0, 8);
+    //        m_xPosition = Random.Range(0, 8);
+
+    //        GameObject spawnArrow = (GameObject)Instantiate(Arrow);
+
+    //        spawnArrow.transform.position = tile.GetGridTilePosition(m_xPosition, m_xPosition);
+
+    //        m_Counter += 1;
+    //    }
+    //}
+
+    //protected void SpawnElectricity()
+    //{
+    //    if (m_Counter <= 1)
+    //    {
+    //        m_xPosition = Random.Range(0, 8);
+    //        m_xPosition = Random.Range(0, 8);
+
+    //        GameObject spawnElectricity = (GameObject)Instantiate(Electricity);
+
+    //        spawnElectricity.transform.position = tile.GetGridTilePosition(m_xPosition, m_xPosition);
+
+    //        m_Counter += 1;
+    //    }
+    //}
+
+
 }
