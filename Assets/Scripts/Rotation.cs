4e718e4bcda6b69c024c9e46de1162a7fe696a6d
Ethan Le <Ethan_le95@hotmail.com>
2017-03-04 22:21:43 -0500

    Added More assets.
    A Cleaner temple and added a power up asset to the game.

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    private float m_Speed = 20f;
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(10 * Time.deltaTime * m_Speed, 0.0f, 0.0f));
+    }
+}
